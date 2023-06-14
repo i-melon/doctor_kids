0eade19c7edf34c3ab9a1f96bb9cbee1942dcba7
i-melon <bazinga010100@gmail.com>
2023-06-14 14:57:29 +0600

    Add firebaseSDK

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:doctor_kids/src/views/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-void main() => runApp(MaterialApp(
-      home: HomePage(),
-    ));
+void main() {
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseDatabase database = FirebaseDatabase.instance;
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
